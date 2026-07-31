9cbd48d8257101f8323f9b0b74e59d56e0a9641e
yuqitao1024 <yuqitao1024@gmail.com>
2026-07-31 16:37:28 +0800

    feat(lightning-indexer): materialize prefill scores

--- src/cannbench/operators/builtin/lightning_indexer/simt/v1/setup.py
+++ src/cannbench/operators/builtin/lightning_indexer/simt/v1/setup.py
@@ -41,6 +41,11 @@ KERNEL_LIBRARIES = {
         "simt",
         "lightning_indexer_prefill_q2_family_64x128.asc",
     ),
+    "liblightning_indexer_prefill_full_score_family_64x128_kernel.so": os.path.join(
+        EXTENSIONS_DIR,
+        "simt",
+        "lightning_indexer_prefill_full_score_family_64x128.asc",
+    ),
     "liblightning_indexer_radix_topk_bfloat16_kernel.so": os.path.join(
         EXTENSIONS_DIR,
         "simt",
